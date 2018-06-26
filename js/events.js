e4aaec5f946f380b14066e9352d38c751f04d999
TylerNodell <tyler.l.nodell@gmail.com>
2018-06-26 22:49:14 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -8,7 +8,7 @@ function getIt() {
 function frameIt() {
   $('img').on('load', function() {
     $(this).addClass('tasty')
-    $(this).css('border', "solid 2px red")
+    $(this).css('border', "solid 100px red")
   })
 }
 
